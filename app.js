1c5c300b639c15f5abca6f01c1934968bb4816a9
kurumkan <joomrise@gmail.com>
2016-11-25 02:35:27 +0800

    console.log api request url

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static("public"));
-
 app.use(function(req, res, next){
 	if(req.headers["x-forwarded-proto"] === "https"){
+		console.log("http://"+req.hostname+req.url)
 		res.redirect("http://"+req.hostname+req.url);
 	}else{		
 		next();
 	}
 });
+
+app.use(express.static("public"));
 
 app.get("/", function(request, response){
 	response.send("Hello from the server!");
