353435b0c7c5eefb471c4c7db6d84ec660a3e3ed
kurumkan <joomrise@gmail.com>
2016-11-25 02:29:55 +0800

    add middleware - all api calls are over http, but not https

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var app = express();
 
 app.use(express.static("public"));
 
+app.use(function(req, res, next){
+	if(req.headers["x-forwarded-proto"] === "https"){
+		res.redirect("http://"+req.hostname+req.url);
+	}else{		
+		next();
+	}
+});
+
 app.get("/", function(request, response){
 	response.send("Hello from the server!");
 });
