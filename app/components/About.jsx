698adbe0e085c10d4e75b9aa1756cdbaadc412fb
kurumkan <joomrise@gmail.com>
2016-11-26 15:23:11 +0800

    style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,21 @@ var React = require("react");
 module.exports = (props) => {
 	return (
 		<div>
-			This is fucking About component
+			<h1 className="text-center">About</h1>
+			<p>
+				This is a weather application built on React
+			</p>	
+			<p>
+				Here are some tools I used:
+			</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react/" target="_blank">React</a>					
+				</li>
+				<li>
+					<a href="https://openweathermap.org/" target="_blank">Open Weather Map</a>					
+				</li>				
+			</ul>
 		</div>
 	);
 }
