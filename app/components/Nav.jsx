b069299b17ef3b0458ef12ee5d06432beb1eb5d1
kurumkan <joomrise@gmail.com>
2016-11-26 14:43:11 +0800

    style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,14 +1,45 @@
 var React = require("react");
 var {Link, IndexLink} = require("react-router");
 
-module.exports = (props)=>{
-	return (
-		<div>
-			<IndexLink to="/" activeClassName="active" >Get Weather</IndexLink>
-			<br/>
-			<Link to="/about" activeClassName="active" >About</Link>
-			<br/>
-			<Link to="/examples" activeClassName="active" >Examples</Link>
-		</div>
-	);
-};
+module.exports = React.createClass({
+	onSearch: function(e){
+		e.preventDefault();
+		alert("submitted!");
+	},
+
+	render: function(){
+		return (
+			<div className="top-bar">
+				<div className="top-bar-left">
+					<ul className="menu">
+						<li className="menu-text">
+							Weather App
+						</li>
+						<li>
+							<IndexLink to="/" activeClassName="active" >Get Weather</IndexLink>
+						</li>
+						<li>
+							<Link to="/about" activeClassName="active" >About</Link>			
+						</li>
+						<li>
+							<Link to="/examples" activeClassName="active" >Examples</Link>			
+						</li>
+					</ul>
+				</div>
+				<div className="top-bar-right">
+					<form onSubmit={this.onSearch}>
+						<ul className="menu">
+							<li>
+								<input type="search" placeholder="Enter city name" />
+							</li>
+							<li>
+								<button type="button" className="button">Go</button>
+							</li>
+						</ul>					
+						
+					</form>
+				</div>
+			</div>
+		);
+	}	
+});
