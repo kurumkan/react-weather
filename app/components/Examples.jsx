6b1561a7636f6b2a465b439051a3ccb9a4518cb0
kurumkan <joomrise@gmail.com>
2016-11-26 15:12:57 +0800

    add grid system & style Examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,21 @@
 var React = require("react");
+var {Link} = require("react-router");
 
 module.exports = (props)=>{
 	return (
 		<div>
-			This is Examples page
+			<h1 className="text-center">Examples</h1>
+			<p>	
+				Here are a few example locations to try out
+			</p>
+			<ol>
+				<li>
+					<Link to="/?location=Kurumkan">Kurumkan</Link>
+				</li>
+				<li>
+					<Link to="/?location=Osaka">Osaka</Link>
+				</li>
+			</ol>			
 		</div>
 	);
 };
